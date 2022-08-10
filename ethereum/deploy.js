ffb9e4b3601f2c0d57a364b86421bde9a89406ec
Rick Ansay <rick.ansay@gmail.com>
2022-08-09 21:58:22 -0600

    Final deployment logic

---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  // Please steal all my ether
+  'false maximum level balance view forum body also alone disorder exist flag',
+  'https://rinkeby.infura.io/v3/9663fa4ca5aa4ea09c71f32b91453ce9'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
