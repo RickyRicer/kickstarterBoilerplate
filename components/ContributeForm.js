af94f4171e289d8c1cfbac4ecaf123de33803ef1
Rick Ansay <rick.ansay@gmail.com>
2022-09-02 21:42:51 -0600

    Communicating campaign address

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
+import Campaign from '../ethereum/campaign';
 
 class ContributeForm extends Component {
   state = {
@@ -8,7 +9,8 @@ class ContributeForm extends Component {
 
   onSubmit = (event) => {
     event.preventDefault();
-    
+
+    const campaign = Campaign(this.props.address);
   };
 
   render() {
